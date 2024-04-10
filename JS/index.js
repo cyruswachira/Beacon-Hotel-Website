ba20e733abbe0598d2873d838e792677531c8963
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-04-10 15:40:46 +0300

    db.json file is added

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+document .addEventListener('DOMContentLoaded', () => {
+
+})
+
+const BASE_URL = 'http://localhost:3000/categories'
+
+function fetchMeals(){
+
+     fetch(`${BASE_URL}`)  
+    
+}
